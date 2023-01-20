429dc7206dd0a0dbaca07f0a153ebf13ea62182d
feddorovich <fedsygreen@mail.ru>
2023-01-20 11:42:49 +0300

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <div>Hello World</div>
+            <div><Rating /></div>
+            <div><Accordion /></div>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <div><Star /></div>
+            <div><Star /></div>
+            <div><Star /></div>
+            <div><Star /></div>
+            <div><Star /></div>
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>
+            <div>Star</div>
+        </div>
+    )
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Меню</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 export default App;
