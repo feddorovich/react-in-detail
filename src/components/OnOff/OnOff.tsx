c69e98ab27302b99c481596daf8549a4053f3eed
feddorovich <feddorovich@outlook.com>
2023-02-08 19:38:09 +0300

    05 conditional rendering hw

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -8,11 +8,8 @@ type OnOffType = {
 export const OnOff = (props: OnOffType) => {
     return (
         <div>
-            {props.value &&
-                <div className={s.on}>ON</div>
-            }
-            {props.value &&
-                <div className={s.off}>OFF</div>}
+            {props.value && <div className={s.on}>ON</div>}
+            {props.value && <div className={s.off}>OFF</div>}
         </div>
     );
 };
