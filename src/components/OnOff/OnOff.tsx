e91b9ac3e912e326591f99ed1035b1bde16eabe3
feddorovich <feddorovich@outlook.com>
2023-02-16 13:49:42 +0300

    05 hw

--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import s from './OnOff.module.css'
 
-type OnOffType = {
-    value: boolean
+type PropsType = {
+    on: boolean
 }
 
-export const OnOff = (props: OnOffType) => {
+const OnOff = (props: PropsType) => {
+
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: props.on ? 'green' : 'white'
+    };
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '2px',
+        padding: '2px',
+        backgroundColor: !props.on ? 'red' : 'white'
+    };
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '5px',
+        backgroundColor: props.on ? 'green' : 'red'
+    };
+
     return (
         <div>
-            {props.value && <div className={s.on}>ON</div>}
-            {props.value && <div className={s.off}>OFF</div>}
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
+
+
+            {/*{props.on && <div className={s.on}>ON</div>}
+            {props.on && <div className={s.off}>OFF</div>}*/}
         </div>
     );
 };
+
+export default OnOff
