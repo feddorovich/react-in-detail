a4503ae9efa6717261ee74d0fa83326108e9901c
feddorovich <feddorovich@outlook.com>
2023-03-26 15:11:05 +0300

    18 useMemo practice

--- src/stories/UseMemoPractice.stories.tsx
+++ src/stories/UseMemoPractice.stories.tsx
@@ -1,44 +1,56 @@
-import React from "react";
+import React, {ChangeEvent, useCallback, useMemo, useState} from "react";
 
 export default {
     title: 'useMemoPractice'
 }
 
-export const UseMemoPractice = () => {
+type citiesType = {country: string, city: string}
 
-    let cities = [
-        {country: 'Belarus' , city: 'Minsk'},
-        {country: 'Belarus' , city: 'Grodno'},
-        {country: 'Belarus' , city: 'Brest'},
-        {country: 'Russia' , city: 'Moscow'},
-        {country: 'Russia' , city: 'St. Petersburg'},
-        {country: 'Russia' , city: 'Rostov'},
-        {country: 'GB' , city: 'London'},
-        {country: 'GB' , city: 'Manchester'},
-        {country: 'GB' , city: 'Liverpool'}
-    ]
+let cities: citiesType[] = [
+    {country: 'Belarus', city: 'Minsk'},
+    {country: 'Belarus', city: 'Grodno'},
+    {country: 'Belarus', city: 'Brest'},
+    {country: 'Russia', city: 'Moscow'},
+    {country: 'Russia', city: 'St. Petersburg'},
+    {country: 'Russia', city: 'Rostov'},
+    {country: 'GB', city: 'London'},
+    {country: 'GB', city: 'Manchester'},
+    {country: 'GB', city: 'Liverpool'}
+]
+
+export const UseMemoPractice = () => {
+    const [selestCity, setSelectCity] = useState('')
+    const selectCityHandle = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+        console.log(1)
+        setSelectCity(e.currentTarget.value)
+    },[])
 
     let belarusCities = [...cities].filter(c => c.country === 'Belarus')
+    const [selestCityBelarus, setSelectCityBelarus] = useState('')
+    const selectBelarusCityHandle = (e: ChangeEvent<HTMLSelectElement>) => {
+        console.log(2)
+        setSelectCityBelarus(e.currentTarget.value)
+    }
 
     return <div>
         <div>
-            <label>All cities: </label>
-            <select id="select-country" value={cities[0].city}>
-                <option disabled>Please choose a country</option>
-                {cities.map(c => {
-                    return <option value={c.city}>{c.city}</option>
-                })}
-            </select>
-        </div>
-        <br/>
-        <div>
-            <label>Belarus cities: </label>
-            <select id="select-country" value={cities[0].city}>
-                <option disabled>Please choose a country</option>
-                {belarusCities.map(c => {
-                    return <option value={c.city}>{c.city}</option>
-                })}
-            </select>
+            <First value={selestCity} onChange={selectCityHandle} list={cities} label ={'All cities:'}/>
+            <br/>
+            <First value={selestCityBelarus} onChange={selectBelarusCityHandle} list={belarusCities} label ={'Belarus cities:'}/>
         </div>
     </div>
 }
+
+const First = React.memo((props: any) => {
+    console.log(props.label)
+    return <div>
+        <label>{props.label}</label>
+        <select id="select-all" value={props.value} onChange={props.onChange}>
+            <option disabled>Please choose a country</option>
+            {props.list.map((c: citiesType, i: number) => {
+                return <option key={i} value={c.city}>{c.city}</option>
+            })}
+        </select>
+    </div>
+})
+
