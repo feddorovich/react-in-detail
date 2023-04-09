df11a92dfb5cb34b0e5da46270c29dd64fe17218
feddorovich <feddorovich@outlook.com>
2023-04-09 19:49:05 +0300

    22 - useEffect, setTimeout, setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -32,3 +32,21 @@ export const SimpleExample = () => {
     );
 };
 
+export const SetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1)
+    const [fake, setFake] = useState(1)
+
+    console.log('SetTimeoutExample')
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(state => state + 1)
+        }, 1000)
+    }, [])
+
+    return (
+        <>
+             Hello, counter: {counter} - fake: {fake}
+        </>
+    );
+};
